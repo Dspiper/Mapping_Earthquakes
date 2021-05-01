4fa7e3c3110f93c2baacada62170601fd65b4eb0
Drake Piper <dspiper@Drakes-MacBook-Pro.local>
2021-05-01 15:21:33 -0700

    Adding updated logic.js file

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,7 +2,27 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 4);
+// let map = L.map('mapid').setView([36.1733, -120.1794], 7);
+
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+// Coordinates for each point to be used in the line.
+// Coordinates for each point to be used in the polyline.
+let line = [
+    [37.6213, -122.3790],
+    [30.197415462580242, -97.66611686561768],
+    [43.67795100805296, -79.62361034645399],
+    [40.64122966858345, -73.7779137969001]
+  ];
+
+  // Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "blue",
+    weight: 4,
+    opacity: .5,
+    dashArray: "5 5"
+  }).addTo(map);
 
 // L.circleMarker([34.0522, -118.2437], {
 //     radius: 300,
@@ -19,7 +39,7 @@ let map = L.map('mapid').setView([34.0522, -118.2437], 4);
 // });
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
